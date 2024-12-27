071717d559bfc2eda1d16ca6ca9b6f769ab14324
sslsl <wuhaoyu@usc.edu>
2024-12-26 17:03:07 -0800

    add main e commerce backend logic

---
+++ src/main/java/com/masai/models/CartItem.java
@@ -0,0 +1,66 @@
+package com.masai.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class CartItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer cartItemId;
+	
+	
+	@OneToOne
+	@JsonIgnoreProperties(value={
+			"productId",
+			"seller",
+			"quantity"
+			
+	})
+	private Product cartProduct;
+	
+	private Integer cartItemQuantity;
+
+	// Getters and Setters
+
+	public Integer getCartItemId() {
+		return cartItemId;
+	}
+
+	public void setCartItemId(Integer cartItemId) {
+		this.cartItemId = cartItemId;
+	}
+
+	public Product getCartProduct() {
+		return cartProduct;
+	}
+
+	public void setCartProduct(Product cartProduct) {
+		this.cartProduct = cartProduct;
+	}
+
+	public Integer getCartItemQuantity() {
+		return cartItemQuantity;
+	}
+
+	public void setCartItemQuantity(Integer cartItemQuantity) {
+		this.cartItemQuantity = cartItemQuantity;
+	}
+}
---
+++ src/main/java/com/masai/models/UserSession.java
@@ -0,0 +1,83 @@
+package com.masai.models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
+public class UserSession {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer sessionId;
+	
+	@Column(unique = true)
+	private String token;
+	
+	@Column(unique = true)
+	private Integer userId;
+	
+	private String userType;
+	
+	private LocalDateTime sessionStartTime;
+	
+	private LocalDateTime sessionEndTime;
+	public Integer getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(Integer sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public LocalDateTime getSessionStartTime() {
+		return sessionStartTime;
+	}
+
+	public void setSessionStartTime(LocalDateTime sessionStartTime) {
+		this.sessionStartTime = sessionStartTime;
+	}
+
+	public LocalDateTime getSessionEndTime() {
+		return sessionEndTime;
+	}
+
+	public void setSessionEndTime(LocalDateTime sessionEndTime) {
+		this.sessionEndTime = sessionEndTime;
+	}
+}
